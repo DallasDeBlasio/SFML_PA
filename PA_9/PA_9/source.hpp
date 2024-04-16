bc09ced42266625fc69a0d22b57b02f0036545ff
Yoilbob <j.richards-perhatch@wsu.edu>
2024-04-15 18:35:15 -0700

    My lib pathways plus moving include

--- PA_9/PA_9/source.hpp
+++ PA_9/PA_9/source.hpp
@@ -1,4 +1,10 @@
+//C : \Panic\SFML - 2.6.1\include //general
+//C:\Panic\SFML - 2.6.1\lib //not general
 
 
 //Dallas DeBlasio C++ general: C:\Users\dalla\OneDrive\Desktop\CPTS 121\SFML-2.6.1\include
 //Dallas DeBlasio linker: C:\Users\dalla\OneDrive\Desktop\CPTS 121\SFML-2.6.1\lib
+
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
