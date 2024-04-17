c45ac88c3b18ae4e01a616c9a7a962a668fa9e9d
Asa Fischer <asafischer42@gmail.com>
2024-04-16 17:13:49 -0700

    new paramerter texture add to character constructor

--- PA_9/PA_9/Character.cpp
+++ PA_9/PA_9/Character.cpp
@@ -8,7 +8,7 @@ Character::Character(int newDamage, int newHP, sf::RectangleShape newRectangle, 
 	this->mSprite = newSprite;
 }
 
-Character::Character(const int& newDamage, const int& newHP, const sf::Vector2f& size, const sf::Vector2f& position, const sf::Vector2f & startIntRect)
+Character::Character(const int& newDamage, const int& newHP, const sf::Vector2f& size, const sf::Vector2f& position, const sf::Vector2f & startIntRect, const sf::Texture texture)
 {
 	this->mDamage = newDamage;
 	this->mHP = newHP;
@@ -16,6 +16,7 @@ Character::Character(const int& newDamage, const int& newHP, const sf::Vector2f&
 	this->mRectangle.setPosition(position);
 	this->mSprite.setTextureRect(sf::IntRect(startIntRect.x, startIntRect.y, startIntRect.x + size.x, startIntRect.y + size.y));
 	this->mSprite.setPosition(position);
+	this->mSprite.setTexture(texture);
 }
 
 void Character::moveCharacter(sf::Vector2f vector)
--- PA_9/PA_9/Character.hpp
+++ PA_9/PA_9/Character.hpp
@@ -7,7 +7,7 @@ class Character
 public:
 	Character(int newDamage, int newHP, sf::RectangleShape newRectangle, sf::Sprite newSprite);
 
-	Character(const int& newDamage, const int& newHP, const sf::Vector2f& size, const sf::Vector2f& position, const sf::Vector2f& startIntRect);
+	Character(const int& newDamage, const int& newHP, const sf::Vector2f& size, const sf::Vector2f& position, const sf::Vector2f& startIntRect, const sf::Texture texture);
 
 	void moveCharacter(sf::Vector2f vector);
 
