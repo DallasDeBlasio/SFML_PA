5bcd5d643b2de802c6a38f22881db28b98ac1537
Connor Chase <connor.chase@wsu.edu>
2024-04-17 16:54:44 -0700

    main menu built. going to try to work it into a base class 'Menu' and derrived class 'MainMenu'. Pushing to save progress for now.

---
+++ PA_9/PA_9/MainMenu.hpp
@@ -0,0 +1,107 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+#define maxMenu 4;
+
+class MainMenu
+{
+public:
+	// constructor
+	MainMenu(double width, double height)
+	{
+		// load font for menu text
+		if (!font.loadFromFile("Jersey20-Regular.ttf"))
+		{
+			std::cout << "Error Loading Font\n";
+		}
+
+		// display different menu options to screen
+
+		// play option
+		menu[0].setFont(font); 
+		menu[0].setFillColor(sf::Color::Blue);
+		menu[0].setString("PLAY");
+		menu[0].setCharacterSize(70);
+		menu[0].setPosition(400, 200);
+
+		// option option
+		menu[1].setFont(font); 
+		menu[1].setFillColor(sf::Color::Blue);
+		menu[1].setString("OPTIONS");
+		menu[1].setCharacterSize(70);
+		menu[1].setPosition(400, 300);
+
+		// about option
+		menu[2].setFont(font);
+		menu[2].setFillColor(sf::Color::Blue);
+		menu[2].setString("ABOUT");
+		menu[2].setCharacterSize(70);
+		menu[2].setPosition(400, 400);
+
+		// exit option
+		menu[3].setFont(font);
+		menu[3].setFillColor(sf::Color::Blue);
+		menu[3].setString("EXIT");
+		menu[3].setCharacterSize(70);
+		menu[3].setPosition(400, 500);
+
+		menuSelected = -1; 
+	}
+	~MainMenu()
+	{
+		std::cout << "menu destroyed" << std::endl; 
+	}
+
+	void draw(sf::RenderWindow& window)
+	{
+		for (int index = 0; index < 4; index++)
+
+		{
+			window.draw(menu[index]);
+		}
+	}
+	void MoveUp()
+	{
+		if (menuSelected - 1 >= 0)
+		{
+			menu[menuSelected].setFillColor(sf::Color::Blue);
+
+			menuSelected--;
+			if (menuSelected == -1)
+			{
+				menuSelected = 2;
+			}
+			menu[menuSelected].setFillColor(sf::Color::Red);
+		}
+	}
+	void MoveDown()
+	{
+		if (menuSelected + 1 <= 4)
+		{
+			menu[menuSelected].setFillColor(sf::Color::Blue);
+			menuSelected++;
+			if (menuSelected == 4)
+			{
+				menuSelected = 0;
+			}
+			menu[menuSelected].setFillColor(sf::Color::Red);
+		}
+	}
+
+	int menuPressed()
+	{
+		return menuSelected;
+	}
+
+	
+private:
+
+	int menuSelected;
+	sf::Font font;
+
+	// shows 4 different menu options to screen
+	sf::Text menu[4];
+
+};
--- PA_9/PA_9/main.cpp
+++ PA_9/PA_9/main.cpp
@@ -1,27 +1,209 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include "MainMenu.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // window for main menu
+    sf::RenderWindow menu(sf::VideoMode(960, 720), "Main Menu (use arrow keys)", sf::Style::Default);
+    MainMenu mainMenu(menu.getSize().x, menu.getSize().y);
 
-    while (window.isOpen())
+    // background
+    sf::RectangleShape menuBackground;
+    menuBackground.setSize(sf::Vector2f(960, 720));
+    sf::Texture menuTexture;
+    menuTexture.loadFromFile("tex_molten_iron_a_256.PNG");
+    menuBackground.setTexture(&menuTexture); 
+
+    while (menu.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        sf::Event event;   
+        while (menu.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
-        }
+            {
+                menu.close();
+            }
+            if (event.type == sf::Event::KeyPressed) 
+            {
+                if (event.key.code == sf::Keyboard::Up)
+                {
+                    mainMenu.MoveUp();
+                    break;
+                }
+                if (event.key.code == sf::Keyboard::Down)
+                {
+                    mainMenu.MoveDown();
+                    break;
+                }
+                if (event.key.code == sf::Keyboard::Return)
+                {
+                    sf::RenderWindow Play(sf::VideoMode(960, 720), "SUPER AWESOME GAME");
+                    sf::RenderWindow Options(sf::VideoMode(960, 720), "OPTIONS");
+                    sf::RenderWindow About(sf::VideoMode(960, 720), "ABOUT");
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+                    int x = mainMenu.menuPressed();
+                    if (x == 0)
+                    {
+                        while (Play.isOpen())
+                        {
+                            sf::Event someEvent;
+                            while (Play.pollEvent(someEvent))
+                            {
+                                if (someEvent.type == sf::Event::Closed)
+                                {
+                                    Play.close();
+                                }
+                                if (someEvent.type == sf::Event::KeyPressed)
+                                {
+                                    if (someEvent.key.code == sf::Keyboard::Escape)
+                                    {
+                                        Play.close();
+                                    }
+                                }
+                            }
+                            Options.close(); 
+                            About.close();
+                            Play.clear();
+                            Play.display(); 
+                        }
+                    }
+                    if (x == 1)
+                    {
+                        while (Options.isOpen())
+                        {
+                            sf::Event someEvent; 
+                            while (Options.pollEvent(someEvent))
+                            {
+                                if (someEvent.type == sf::Event::Closed)
+                                {
+                                    Options.close();
+                                }
+                                if (someEvent.type == sf::Event::KeyPressed)
+                                {
+                                    if (someEvent.key.code == sf::Keyboard::Escape)
+                                    {
+                                        Options.close();
+                                    }
+                                }
+                            }
+                            Play.close();
+                            Options.clear();
+                            About.close();
+                            Play.display();
+                        }
+                    }
+                    if (x == 2)
+                    {
+                        while (About.isOpen())
+                        {
+                            sf::Event someEvent;
+                            while (About.pollEvent(someEvent))
+                            {
+                                if (someEvent.type == sf::Event::Closed)
+                                {
+                                    About.close();
+                                }
+                                if (someEvent.type == sf::Event::KeyPressed)
+                                {
+                                    if (someEvent.key.code == sf::Keyboard::Escape)
+                                    {
+                                        About.close();
+                                    }
+                                }
+                            }
+                            Play.close();
+                            Options.close();
+                            About.clear();
+                            About.display();
+                        }
+                    }
+                    if (x == 3)
+                    {
+                        menu.close();
+                        break;
+                    }
+                }
+            }
+            menu.clear();
+            menu.draw(menuBackground); 
+            mainMenu.draw(menu);
+            menu.display(); 
+        }
     }
 
-    return 0;
 
-    //test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //sf::Font font;
+    //sf::Text text;
+    //font.loadFromFile("Jersey20-Regular.ttf");
+    //text.setFont(font);    
+    //text.setCharacterSize(30);
+    //text.setString("Hello world");
+    //text.setStyle(sf::Text::Regular); 
+    //text.setPosition(sf::Vector2f(500, 500));   
+    //text.setFillColor(sf::Color::Red);  
+
+    //sf::RenderWindow window(sf::VideoMode(2560, 1600), "SFML works!");
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
+
+    //while (window.isOpen())
+    //{
+    //    sf::Event event;
+    //    while (window.pollEvent(event))
+    //    {
+    //        if (event.type == sf::Event::Closed)
+    //            window.close();
+    //    }
+
+    //    window.clear();
+    //    //window.draw(shape); 
+
+    //    window.draw(text);
+
+    //   window.display();
+
+    //}
+
+    //return 0;
+
+    ////test
 }
