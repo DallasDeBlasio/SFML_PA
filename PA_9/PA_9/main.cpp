c587141a82742bbaa963bed60c46be1476a5997c
Yoilbob <j.richards-perhatch@wsu.edu>
2024-04-17 08:42:30 -0700

    Got hero on screen and found the difference between animation frames

--- PA_9/PA_9/main.cpp
+++ PA_9/PA_9/main.cpp
@@ -32,9 +32,32 @@ int main()
     //Character(const int& newDamage, const int& newHP, const sf::Vector2f & size, const sf::Vector2f & position, const sf::Vector2f & startIntRect, const sf::Texture texture);
 
     sf::Texture hero;
-    hero.loadFromFile("tempHero.png");
 
-    Character todd(15, 30, sf::Vector2f(30, 30), origin, sf::Vector2f(0, 0), hero);
+    hero.loadFromFile("Assets/tempHero.png");
+
+
+
+    int one = 16;
+    int two = 9;
+    int three = 16;
+    int four = 31;
+
+    //48 difference in height 
+
+    sf::Sprite herotest;
+    herotest.setTextureRect(sf::IntRect(one+48, two+48, three, four));
+    herotest.setTexture(hero);
+    herotest.setPosition(0.f, 0.f);
+    herotest.setScale(10.f, 10.f);
+    sf::FloatRect herotestbounds = herotest.getGlobalBounds();
+    sf::RectangleShape herotestboundsDrawable(herotestbounds.getSize());
+
+
+   // Character todd(15, 30, sf::Vector2f(30, 30), origin, sf::Vector2f(0, 0), hero);
+    Character todd(15, 30, sf::Vector2f(100, 100), sf::Vector2f(30, 30), sf::Vector2f(0, 0), hero);
+   
+
+    todd.mRectangle.setFillColor(sf::Color::Black);
 
 
 
@@ -53,6 +76,10 @@ int main()
         window.draw(todd.mRectangle);
         window.draw(todd.mSprite);
 
+        window.draw(herotestboundsDrawable);
+        window.draw(herotest);
+
+
 
         window.display();
 
@@ -61,7 +88,7 @@ int main()
 
     }
     //loud noises
-    return 0;
+    return 14;
 
     //test
 }
