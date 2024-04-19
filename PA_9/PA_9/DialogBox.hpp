92a29b981b925dc7cfefc31784a1da97079161cc
Connor Chase <connor.chase@wsu.edu>
2024-04-18 20:06:36 -0700

    moved menu out of main and into object

--- PA_9/PA_9/DialogBox.hpp
+++ PA_9/PA_9/DialogBox.hpp
@@ -4,27 +4,18 @@
 
 /// <summary>
 /// Generates border/box around text. Use inherited functions from sf::Text to change position of box, change color of text, etc..
-/// Must call window.draw(box.getBox) and window.draw(box.getText) before displaying.
+/// Must call window.draw(box.getBox) and window.draw(box.getText) before displaying. Text boxes are invisible and are not centered perfectly around text; ues offset functions to center box.
 /// </summary>
 class DialogBox : public sf::Text, sf::RectangleShape   
 {
 public:
-
-	//DialogBox()
-	//{
-	//	stringToDisplay = sf::String(); 
-	//	text = sf::Text(); 
-	//	font = sf::Font(); 
-	//	bounds = sf::FloatRect(); 
-	//	box = sf::RectangleShape(); 
-	//}
 
 	DialogBox()
 	{
 		font.loadFromFile("Jersey20-Regular.ttf");
 		text.setFont(font);
 		text.setFillColor(sf::Color::Blue);
-		text.setString("Default String blah\n bdskhfdhfsa;lkjdflsd");
+		text.setString("Default String");
 		text.setCharacterSize(40);
 		text.setPosition(400, 200);
 
@@ -32,11 +23,14 @@ public:
 		bounds = text.getLocalBounds();  
 		position = text.getPosition();
 
+		bounds.height * text.getCharacterSize();
+		bounds.width* text.getCharacterSize();
 
 		box.setPosition((position.x - 9), position.y); 
 		box.setSize(sf::Vector2f((bounds.width + 20), (bounds.height + 23)));
-		box.setFillColor(sf::Color::Red);
+		box.setFillColor(sf::Color::Yellow);
 
+		std::cout << "loaded Default dialog box" << std::endl; 
 	}
 	DialogBox(sf::String stringToDisplay)
 	{
@@ -48,13 +42,19 @@ public:
 		text.setPosition(400, 200);
 
 
-		bounds = text.getGlobalBounds(); 
+		bounds = text.getLocalBounds(); 
+		position = text.getPosition();
 
+		bounds.height* text.getCharacterSize();
+		bounds.width* text.getCharacterSize();
 		
-		box.setPosition(text.getPosition()); 
-		box.setSize(sf::Vector2f(bounds.width, bounds.height));
-		box.setFillColor(sf::Color::Red);
+		box.setPosition((position.x - 9), position.y);  
+		box.setSize(sf::Vector2f((bounds.width + 20), (bounds.height + 23)));
+		box.setFillColor(sf::Color::Yellow);
+
+		std::string stringForPrintDebugging = stringToDisplay; 
 		
+		std::cout << "loaded dialog box with \"" << stringForPrintDebugging << "\"" << std::endl;  
 	}
 
 	sf::RectangleShape getBox()
@@ -71,7 +71,64 @@ public:
 		font.loadFromFile(filePath); 
 	}
 
-
+	/// <summary>
+	/// distance in pixels to offset rectangle to the right
+	/// </summary>
+	void offsetRight(float distance)
+	{
+		box.setPosition((position.x - distance), position.y);
+		position.x = position.x - distance; 
+	}
+	/// <summary>
+	/// distance in pixels to offset bos to left
+	/// </summary>
+	void offsetLeft(float distance)
+	{
+		box.setPosition((position.x + distance), position.y);
+		position.x = position.x + distance;
+	}
+	/// <summary>
+	/// distance in pixels to offset upwads
+	/// </summary>
+	void offsetUp(float distance)
+	{
+		box.setPosition(position.x, (position.y + distance));
+		position.y = position.y + distance;
+	}
+	/// <summary>
+	/// distance in pixels to offset downwards
+	/// </summary>
+	void offsetDown(float distance)
+	{
+		box.setPosition(position.x, (position.y - distance));
+		position.y = position.y - distance;
+	}
+	/// <summary>
+	/// change the texture of the box
+	/// </summary>
+	/// <param name="texture"></param>
+	void changeTexture(sf::Texture* texture )
+	{
+		box.setTexture(texture);
+	}
+	/// <summary>
+	/// adjust the height of the box. +num to increase, -num to decrease size
+	/// </summary>
+	/// <param name="difference"></param>
+	void changeHeight(float difference)
+	{
+		sf::Vector2f dimensions = box.getSize(); 
+		box.setSize(sf::Vector2f(dimensions.x, (dimensions.y + difference)));
+	}
+	/// <summary>
+	/// adjust the height of the box. +num to increase, -num to decrease size
+	/// </summary>
+	/// <param name="difference"></param>
+	void changeWidth(float difference)
+	{
+		sf::Vector2f dimensions = box.getSize(); 
+		box.setSize(sf::Vector2f((dimensions.x + difference), dimensions.y)); 
+	}
 private:
 
 	sf::RectangleShape box;
--- PA_9/PA_9/MainMenu.hpp
+++ PA_9/PA_9/MainMenu.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "DialogBox.hpp"
 
 
 #define maxMenu 4;
@@ -8,94 +9,407 @@
 class MainMenu
 {
 public:
-	// constructor
-	MainMenu(double width, double height)
-	{
-		// load font for menu text
-		if (!font.loadFromFile("Jersey20-Regular.ttf"))
-		{
-			std::cout << "Error Loading Font\n";
-		}
+    // constructor
+    MainMenu(double width, double height)
+    {
+        // load font for menu text
+        if (!font.loadFromFile("Jersey20-Regular.ttf"))
+        {
+            std::cout << "Error Loading Font\n";
+        }
 
-		// display different menu options to screen
+        // display different menu options to screen
 
-		// play option
-		menu[0].setFont(font); 
-		menu[0].setFillColor(sf::Color::Blue);
-		menu[0].setString("PLAY");
-		menu[0].setCharacterSize(70);
-		menu[0].setPosition(400, 200);
+        // play option
+        menu[0].setFont(font);
+        menu[0].setFillColor(sf::Color::Blue);
+        menu[0].setString("PLAY");
+        menu[0].setCharacterSize(70);
+        menu[0].setPosition(400, 200);
 
-		// option option
-		menu[1].setFont(font); 
-		menu[1].setFillColor(sf::Color::Blue);
-		menu[1].setString("OPTIONS");
-		menu[1].setCharacterSize(70);
-		menu[1].setPosition(400, 300);
+        // option option
+        menu[1].setFont(font);
+        menu[1].setFillColor(sf::Color::Blue);
+        menu[1].setString("OPTIONS");
+        menu[1].setCharacterSize(70);
+        menu[1].setPosition(400, 300);
 
-		// about option
-		menu[2].setFont(font);
-		menu[2].setFillColor(sf::Color::Blue);
-		menu[2].setString("ABOUT");
-		menu[2].setCharacterSize(70);
-		menu[2].setPosition(400, 400);
+        // about option
+        menu[2].setFont(font);
+        menu[2].setFillColor(sf::Color::Blue);
+        menu[2].setString("ABOUT");
+        menu[2].setCharacterSize(70);
+        menu[2].setPosition(400, 400);
 
-		// exit option
-		menu[3].setFont(font); 
-		menu[3].setFillColor(sf::Color::Blue);
-		menu[3].setString("EXIT");
-		menu[3].setCharacterSize(70);
-		menu[3].setPosition(400, 500);
+        // exit option
+        menu[3].setFont(font);
+        menu[3].setFillColor(sf::Color::Blue);
+        menu[3].setString("EXIT");
+        menu[3].setCharacterSize(70);
+        menu[3].setPosition(400, 500);
 
-		menuSelected = -1; 
-	}
-	~MainMenu()
-	{
+        menuSelected = -1;
+    }
+    ~MainMenu()
+    {
 
-	}
+    }
 
-	void draw(sf::RenderWindow& window)
-	{
-		for (int index = 0; index < 4; index++)
+    void draw(sf::RenderWindow& window)
+    {
+        for (int index = 0; index < 4; index++)
 
-		{
-			window.draw(menu[index]);
-		}
-	}
-	void MoveUp()
-	{
-		if (menuSelected - 1 >= 0)
-		{
-			menu[menuSelected].setFillColor(sf::Color::Blue);
+        {
+            window.draw(menu[index]);
+        }
+    }
+    void MoveUp()
+    {
+        if (menuSelected - 1 >= 0)
+        {
+            menu[menuSelected].setFillColor(sf::Color::Blue);
 
-			menuSelected--;
-			if (menuSelected == -1)
-			{
-				menuSelected = 2;
-			}
-			menu[menuSelected].setFillColor(sf::Color::Red);
-		}
-	}
-	void MoveDown()
-	{
-		if (menuSelected + 1 <= 4)
-		{
-			menu[menuSelected].setFillColor(sf::Color::Blue);
-			menuSelected++;
-			if (menuSelected == 4)
-			{
-				menuSelected = 0;
-			}
-			menu[menuSelected].setFillColor(sf::Color::Red);
-		}
-	}
+            menuSelected--;
+            if (menuSelected == -1)
+            {
+                menuSelected = 2;
+            }
+            menu[menuSelected].setFillColor(sf::Color::Red);
+        }
+    }
+    void MoveDown()
+    {
+        if (menuSelected + 1 <= 4)
+        {
+            menu[menuSelected].setFillColor(sf::Color::Blue);
+            menuSelected++;
+            if (menuSelected == 4)
+            {
+                menuSelected = 0;
+            }
+            menu[menuSelected].setFillColor(sf::Color::Red);
+        }
+    }
 
-	int menuPressed()
-	{
-		return menuSelected;
-	}
+    int menuPressed()
+    {
+        return menuSelected;
+    }
+
+    /// <summary>
+    /// runs main menu plus other windows
+    /// </summary>
+    //void mainMenuAll()
+    //{
+    //   // DialogBox box("Hello World");
+    //    // DialogBox box;
+
+
+
+    //    // window for main menu
+    //    sf::RenderWindow menu(sf::VideoMode(960, 720), "Main Menu (use arrow keys)", sf::Style::Default);
+    //    MainMenu mainMenu(menu.getSize().x, menu.getSize().y);
+
+    //    // background
+    //    sf::RectangleShape menuBackground;
+    //    menuBackground.setSize(sf::Vector2f(960, 720));
+    //    sf::Texture menuTexture;
+    //    menuTexture.loadFromFile("tex_molten_iron_a_256.PNG");
+    //    menuBackground.setTexture(&menuTexture);
+
+
+    //    while (menu.isOpen())
+    //    {
+    //        sf::Event event;
+    //        while (menu.pollEvent(event))
+    //        {
+    //            if (event.type == sf::Event::Closed)
+    //            {
+    //                menu.close();
+    //            }
+    //            if (event.type == sf::Event::KeyPressed)
+    //            {
+    //                if (event.key.code == sf::Keyboard::Up)
+    //                {
+    //                    mainMenu.MoveUp();
+    //                    break;
+    //                }
+    //                if (event.key.code == sf::Keyboard::Down)
+    //                {
+    //                    mainMenu.MoveDown();
+    //                    break;
+    //                }
+    //                if (event.key.code == sf::Keyboard::Return)
+    //                {
+    //                    sf::RenderWindow Play(sf::VideoMode(960, 720), "SUPER AWESOME GAME");
+    //                    sf::RenderWindow Options(sf::VideoMode(960, 720), "OPTIONS");
+    //                    sf::RenderWindow About(sf::VideoMode(960, 720), "ABOUT");
+
+    //                    int x = mainMenu.menuPressed();
+    //                    if (x == 0)
+    //                    {
+    //                        while (Play.isOpen())
+    //                        {
+    //                            // GAME WINDOW --- GAME LOOP HERE 
+
+    //                            sf::Event someEvent;
+    //                            while (Play.pollEvent(someEvent))
+    //                            {
+    //                                if (someEvent.type == sf::Event::Closed)
+    //                                {
+    //                                    Play.close();
+    //                                }
+    //                                if (someEvent.type == sf::Event::KeyPressed)
+    //                                {
+    //                                    if (someEvent.key.code == sf::Keyboard::Escape)
+    //                                    {
+    //                                        Play.close();
+    //                                    }
+    //                                }
+    //                            }
+    //                           // Play.draw(box.getBox());
+    //                            //Play.draw(box.getText());
+    //                            Play.display();
+    //                            Options.close();
+    //                            About.close();
+    //                            Play.clear();
+    //                        }
+    //                    }
+    //                    if (x == 1)
+    //                    {
+    //                        while (Options.isOpen())
+    //                        {
+    //                            sf::Event someEvent;
+    //                            // OPTIONS WINDOW
+    //                            while (Options.pollEvent(someEvent))
+    //                            {
+    //                                if (someEvent.type == sf::Event::Closed)
+    //                                {
+    //                                    Options.close();
+    //                                }
+    //                                if (someEvent.type == sf::Event::KeyPressed)
+    //                                {
+    //                                    if (someEvent.key.code == sf::Keyboard::Escape)
+    //                                    {
+    //                                        Options.close();
+    //                                    }
+    //                                }
+    //                            }
+    //                            Play.close();
+    //                            Options.clear();
+    //                            About.close();
+    //                            Play.display();
+    //                        }
+    //                    }
+    //                    if (x == 2)
+    //                    {
+    //                        while (About.isOpen())
+    //                        {
+    //                            sf::Event someEvent;
+    //                            // ABOUT WINDOW
+    //                            while (About.pollEvent(someEvent))
+    //                            {
+    //                                if (someEvent.type == sf::Event::Closed)
+    //                                {
+    //                                    About.close();
+    //                                }
+    //                                if (someEvent.type == sf::Event::KeyPressed)
+    //                                {
+    //                                    if (someEvent.key.code == sf::Keyboard::Escape)
+    //                                    {
+    //                                        About.close();
+    //                                    }
+    //                                }
+    //                            }
+    //                            Play.close();
+    //                            Options.close();
+    //                            About.clear();
+    //                            About.display();
+    //                        }
+    //                    }
+    //                    if (x == 3)
+    //                    {
+    //                        menu.close();
+    //                        break;
+    //                    }
+    //                }
+    //            }
+    //            menu.clear();
+    //            menu.draw(menuBackground);
+    //            mainMenu.draw(menu);
+    //            menu.display();
+    //        }
+    //    }
+    //}
+
+    void runPlayWindow() 
+    { 
+        sf::RenderWindow Play(sf::VideoMode(960, 720), "SNAIL GAME SNAIL GAME");
+        DialogBox box("hello world");
+
+        std::cout << "Opened runPlayWindow" << std::endl; 
+
+        while (Play.isOpen())
+        {
+            sf::Event someEvent;
+            while (Play.pollEvent(someEvent))
+            {
+                if (someEvent.type == sf::Event::Closed)
+                {
+                    Play.close();
+                    std::cout << "Closed runPlayWindow" << std::endl;
+                }
+                if (someEvent.type == sf::Event::KeyPressed)
+                {
+                    if (someEvent.key.code == sf::Keyboard::Escape)
+                    {
+                        Play.close();
+                        std::cout << "Closed runPlayWindow" << std::endl;
+                    }
+                }
+            }
+
+            // GAME WINDOW --- GAME LOOP HERE 
+
+
+            Play.draw(box.getBox());
+            Play.draw(box.getText());
+            Play.display();
+            Play.clear();
+        }
+    }
+    void runOptionsWindow()
+    {
+        sf::RenderWindow Options(sf::VideoMode(960, 720), "Options Window");
+
+        std::cout << "Opened runOptionsWindow" << std::endl;
+
+        while (Options.isOpen()) 
+        {
+            sf::Event someEvent;
+            while (Options.pollEvent(someEvent))
+            {
+                if (someEvent.type == sf::Event::Closed)
+                {
+                    Options.close();
+                    std::cout << "Closed runOptionsWindow" << std::endl;
+                }
+                if (someEvent.type == sf::Event::KeyPressed)
+                {
+                    if (someEvent.key.code == sf::Keyboard::Escape)
+                    {
+                        Options.close();
+                        std::cout << "Closed runOptionsWindow" << std::endl;
+                    }
+                }
+            }
+            Options.display();
+            Options.clear();
+        }
+    }
+    void runAboutWindow()
+    {
+        sf::RenderWindow About(sf::VideoMode(960, 720), "About window");
+
+        std::cout << "Opened runAboutWindow" << std::endl;
+
+        while (About.isOpen()) 
+        {
+            sf::Event someEvent;
+            while (About.pollEvent(someEvent))
+            {
+                if (someEvent.type == sf::Event::Closed)
+                {
+                    About.close();
+                    std::cout << "Closed runAboutWindow" << std::endl;
+                }
+                if (someEvent.type == sf::Event::KeyPressed)
+                {
+                    if (someEvent.key.code == sf::Keyboard::Escape)
+                    {
+                        About.close();
+                        std::cout << "Closed runAboutWindow" << std::endl;
+                    }
+                }
+            }
+            About.display();
+            About.clear();
+        }
+    }
 
 	
+    void mainMenuWindow()
+    {
+        // window for main menu
+        sf::RenderWindow menu(sf::VideoMode(960, 720), "Main Menu (use arrow keys)", sf::Style::Default); 
+        MainMenu mainMenu(menu.getSize().x, menu.getSize().y);
+
+        // background
+        sf::RectangleShape menuBackground;
+        menuBackground.setSize(sf::Vector2f(960, 720));
+        sf::Texture menuTexture;
+        menuTexture.loadFromFile("tex_molten_iron_a_256.PNG");
+        menuBackground.setTexture(&menuTexture);
+
+        int x = 0;
+
+        std::cout << "Opened mainMenuWindow" << std::endl;
+
+        while (menu.isOpen())
+        {
+            sf::Event event;
+            while (menu.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                {
+                    menu.close();
+                }
+                if (event.type == sf::Event::KeyPressed)
+                {
+                    if (event.key.code == sf::Keyboard::Up)
+                    {
+                        mainMenu.MoveUp();
+                        break;
+                    }
+                    if (event.key.code == sf::Keyboard::Down)
+                    {
+                        mainMenu.MoveDown();
+                        break;
+                    }
+                    if (event.key.code == sf::Keyboard::Return)
+                    {
+                         x = mainMenu.menuPressed();
+                        // Play selected. open play window 
+                        if (x == 0)
+                        {
+                            runPlayWindow(); 
+                            //menu.close();
+                        }
+                        if (x == 1)
+                        {
+                            runOptionsWindow();
+                        }
+                        if (x == 2)
+                        {
+                            runAboutWindow();
+                        }
+                        if (x == 3)
+                        {
+                            menu.close();
+                            break;
+
+                        }
+                    }
+
+                }            
+                    menu.clear();
+                    menu.draw(menuBackground);
+                    mainMenu.draw(menu);
+                    menu.display();
+            }
+        }
+    }
+
 private:
 
 	int menuSelected;
@@ -104,4 +418,5 @@ private:
 	// shows 4 different menu options to screen
 	sf::Text menu[4];
 
+   
 };
