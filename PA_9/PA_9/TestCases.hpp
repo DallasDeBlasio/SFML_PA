9bb515e87fbdf7e29d3a7c8adf325abb48ecaf5f
Connor Chase <connor.chase@wsu.edu>
2024-04-22 17:10:34 -0700

    test cases class has been started. probably some other stuff too

---
+++ PA_9/PA_9/TestCases.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "MainMenu.hpp"
+#include <iostream>
+class TestCases
+{
+public:
+	TestCases()
+	{
+
+	}
+
+	// READ ME
+	// make sure your test functions return bool. Connor will take care of diplaying messages to the screen 
+
+	bool testWindow(void)
+	{
+	
+		return true;
+	}
+
+private:
+
+
+
+};
